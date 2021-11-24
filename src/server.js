02fa71ae2831a3318bd72d1080d51ce9daf833df
MIN SIK CHOI <minstones34@gmail.com>
2021-11-25 01:23:47 +0900

    updated pakcage-json

---
+++ src/server.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// const app = express();
+//이렇게 express를 실행할 수는 있음
+//하지만 babel에 도움을 받아 보다 최신 문법을 이용가능
+// import express from "express";
+//npm run dev으로 코드를 터미널에서 실행할 수 있다.
+//다만 수정할 때마다 일일히 npm run dev를 쳐줘야 하는 단점이 있음
+//nodemon을 사용하면 한번만 써도 자동으로 재실행해줌
+
+console.log("hiiiiiiiiiiiiiiiiii");
