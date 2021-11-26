b4026e2741d5f706d23d2da4d824e002af215bcf
MIN SIK CHOI <minstones34@gmail.com>
2021-11-26 20:49:46 +0900

    created routers

---
+++ README.md
@@ -0,0 +1,25 @@
+# Wetube Reloaded
+
+# global router 홈에서 바로 갈 수 있는 페이지를 담고 있다
+
+# 원래 라우터 논리대로라면 /users/join /users/login이 맞음 하지만 몇 가지 예외 사항이 있을 수 있으며, 이는 url을 보다 간결하고 이쁘게 만들면서 쉽게 마케팅하기 위해 몇 가지는 예외로 둘 수도 있음.
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+# users router
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+# videos router
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
+
+# 라우터란 /delete-video /edit-video가 아닌 각 url을 그룹화해서 정리할 수 있게 도와준다. /video/delete /video/edit 처럼
--- src/server.js
+++ src/server.js
@@ -6,20 +6,31 @@ const PORT = 4000;
 const app = express(); //어플리케이션을 만듬
 const logger = morgan("dev");
 
-//이곳에서 어플리케이션을 설정한다.
-const home = (req, res) => {
-  return res.send("hello");
-};
-const login = (req, res) => {
-  return res.send("login");
-};
+//여기서 어플리케이션을 설정한다.
+app.use(logger); // app.use()는 그어떤 url에도 use할 수 있는 글로벌미들웨어를 만들어줌. app.get위에 두지 않으면 초기 라우터엔 작동안하니 유의
 
-// app.use(logger) 그어떤 url에도 작동하는 미들웨어를 만들어줌. app.get위에 두지 않으면 초기 라우터엔 작동안하니 유의
-app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+const globalRouter = express.Router(); //이렇게 하면 라우터를 만들 수 있다 아주 간단함.
 
-//이곳에서 어플리케이션을 설정한다.
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter); //누군가가 "/"로 시작하는 url에 접근한다면. globalRouter라는 라우터(정확히는 라우터 안의 컨트롤러)를 use한다
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+//여기까지 어플리케이션을 설정한다.
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
