45193ac08504723ca6c6d27b33b8c57307cc2b25
MIN SIK CHOI <minstones34@gmail.com>
2021-11-27 22:45:08 +0900

    Update videoRouter.js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,10 +4,9 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
 
-export default videoRouter; //js에선 모든 파일이 각각 독립된 모듈이므로 일일히 import 해주는 것과 동시에 export 해줘야 한다.
-//default export는 누군가가 이 파일을 통째로 import하게 될 때, 그때 export하게 될 단 하나의 결과를 디폴트로 적용하는 것.
+export default videoRouter;
