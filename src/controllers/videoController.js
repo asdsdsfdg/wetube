c28a3c95cca600604e2b2924fdbff6a5488ff0fc
MIN SIK CHOI <minstones34@gmail.com>
2021-12-01 00:34:46 +0900

    set Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,12 @@
-// export const trending = (req, res) =>
-//   res.send(
-//     "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
-//   );
-// 저딴 방식으로 html을 리턴할 수는 없다.
-//따라서 간편하게 뷰 엔진의 일종인 pug를 사용해 템플릿 파일을 pug로 보내서 pug가 이를 html 형식으로 변환해 이곳으로 가져오도록 하자.
+//5.7
 
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); //view(템플릿)의 이름을 넣어주는데 여기선 home.pug니까 home을 넣어준다.
-//템플릿을 렌더링하는 건 컨트롤러이므로 변수를 넣는것도 여기서 넣어야함. 렌더링 함수의 두번째인수에 마음껏 pug에서 설정한 변수의 값를 넣어줄 수 있음. {a: "xfd", b: "dfdf"}
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
@@ -14,5 +14,3 @@ export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
 };
-//export default는 한 파일에 하나 밖에 없는 데다, 하나의 함수만을 export 할 수 있다.
-//따라서 export const로 직접 바로 하나씩 export 해주는 것. 이 경우 하나만 있는 default와는 다르기에 import시 이름을 바꾸면 못알아 먹는다.
