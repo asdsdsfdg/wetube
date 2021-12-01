93693eb905d7b0987a8e0bc2d844db0a01258cce
MIN SIK CHOI <minstones34@gmail.com>
2021-12-01 22:22:18 +0900

    remove comment

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,14 +36,12 @@ export const watch = (req, res) => {
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1]; //배열의 시작은 0부터이기 때문.
+  const video = videos[id - 1];
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  // 얘 자체로는 우리의 express 앱이 form의 body을 이해 못한다. 따라서 server.js에 앱이 이해할 수 있도록 해주는 express의 method, urlencoded를 추가해줘야한다.
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
-  // res.redirect는 브라우저가 라디이렉트(자동으로 이동)하게 하는 것이다 브라우저는 우리가 준 그 url로 리다이렉트할거다
 };
--- src/server.js
+++ src/server.js
@@ -14,8 +14,8 @@ app.set("view engine", "pug");
 
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true })); // 라우터를 사용하기 전에 해당 미들웨어를 사용해야한다. 미들웨어가 폼의 value들을 이해하고 그걸 js로 변형하여 라우터에서 사용할 수 있게 해줘야 하기 때문
-// extended 옵션은 폼의 바디에 있는 정보를 보기 좋게 형식을 갖춰주는 일을 함.
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
