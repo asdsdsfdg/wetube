dae0e42ca12b37b56e089f55931fa6a96765ea50
MIN SIK CHOI <minstones34@gmail.com>
2021-12-01 19:14:20 +0900

    updated lteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,20 @@
 //5.7
 
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+//literation은 기본적으로 원소들의 list를 보여주는 것이다. 코멘드들의 list, 비디오들의 list등등
+//literation엔 리스트, 즉 array 또는 object가 필요하다.
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const trending = (req, res) =>
