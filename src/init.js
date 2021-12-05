4277539bb5f3f0cb8a0371e06e4b56bf1f3bfc19
MIN SIK CHOI <minstones34@gmail.com>
2021-12-06 00:27:02 +0900

    created model.find()

--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 import "./db";
-import "./models/video";
-import app from "./server";
+import "./models/Video";
+import app from "./Server";
 
 const PORT = 4000;
 
@@ -8,3 +8,5 @@ const handleListening = () =>
   console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
+
+//server.js에서는 서버에 관련된 것만. 여기는 파일을 모두 import해야하는 것들을 import하고 여기에 이상이 없다면 app을 실행시키는 역할로 구분
