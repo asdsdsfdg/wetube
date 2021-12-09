53e550f3fc435c6ccb5a0bef80d0f3a6b8d8f6be
MIN SIK CHOI <minstones34@gmail.com>
2021-12-10 01:14:48 +0900

    Saved first obj in db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,16 @@
 import Video from "../models/Video";
 
+/*
+console.log("start")
+Video.find({}, (error, videos) => {
+  if(error){
+    return res.render("server-error")
+  }
+  return res.render("home", { pageTitle: "Home", videos });
+});
+console.log("finished")
+*/
+
 export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
@@ -23,9 +34,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
-  const { title } = req.body;
-  videos.push(newVideo);
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  console.log(title, description, hashtags);
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
   return res.redirect("/");
 };
